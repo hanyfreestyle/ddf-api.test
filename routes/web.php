1ed82b4faa113ac6a43f4480c82cc34b8f691be1
Hany Darwish <hany.freestyle4u@gmail.com>
2025-05-23 18:48:34 +0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\DdfApiService;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,11 @@ Route::get('/', function () {
 Route::get('/test-http', function () {
     $res = Http::get('https://jsonplaceholder.typicode.com/posts/1');
     return $res->json();
+});
+
+Route::get('/ddf-test', function (DdfApiService $ddf) {
+    $data = $ddf->getOpenHouses();
+    return response()->json($data);
 });
 
 
